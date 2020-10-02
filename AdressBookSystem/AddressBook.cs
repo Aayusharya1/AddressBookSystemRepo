2911128f6112224ecd0f7b679bca2f4244d30407
aayusharya1 <aayusharya07@gmail.com>
2020-10-03 05:01:39 +0530

    [Aayush] Add. Code for adding a new contactin the address book. 2nd time

---
+++ AdressBookSystem/AddressBook.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    class AddressBook
+    {
+        public static List<Contact> contactBook;
+        public AddressBook()
+        {
+            contactBook = new List<Contact>();
+        }
+
+        public Contact FindContact(string fname)
+        {
+            Contact contact = contactBook.Find((x) => x.firstName == fname);
+            return contact;
+        }
+
+        public bool AddContact(string firstName, string lastName, string address, string city, string state, string zip, string phoneNumber, string email)
+        {
+            Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+            Contact result = FindContact(firstName);
+            if (result == null)
+            {
+                contactBook.Add(contact);
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
